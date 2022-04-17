756cb44e23c7a0895bfbbd21e4947c56ca8a75c0
Marcus Vinicius Abdalla <93200321+MarcusViniciusAbdalla@users.noreply.github.com>
2022-04-16 21:04:04 -0300

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const express = require('express');
 
 const app = express();
 
-app.get('/' , (req , res) => res.send("Olá Mundo!"))
+app.get('/' , (req , res) => res.send("Olá Mundo!"));
 
-app.listen(3000, () => console.log("Servidor rodando na porta 3000"))
+app.listen(3000, () => console.log("Servidor rodando na porta 3000"));
